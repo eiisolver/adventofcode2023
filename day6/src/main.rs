d20184203a378272473a5ba5ceeea82055d1115c
Louis Verhaard <louis@shortestpath.se>
2023-12-05 22:16:47 +0100

    Revert "Small improvement day6"
    This reverts commit a5c8ec3efdb06141d4c1229be220632d46cc0c3f.

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,13 +1,12 @@
-use num_integer::Roots;
 use std::fs;
 use regex::Regex;
 
-fn ways_to_win(time: i64, dist: i64) -> i64 {
-    let v = 4 * time * time - 16 * (dist + 1);
-    time % 2 + v.sqrt() / 2
+fn ways_to_win(time: f64, dist: f64) -> i64 {
+    let root1: f64 = (time - (time * time - 4.0 * dist).sqrt()) / 2.0;
+    (time - 2.0 * root1.floor() - 1.0) as i64
 }
 fn main() {
-    let mut input = fs::read_to_string("test_input.txt").unwrap();
+    let mut input = fs::read_to_string("input.txt").unwrap();
     let re = Regex::new(r"\d+").unwrap();
     for part in 1..=2 {
         let numbers: Vec<i64> = re.find_iter(&input)
@@ -15,7 +14,7 @@ fn main() {
             .collect();
         let n = numbers.len() / 2;
         let result: i64 = (0..n)
-            .map(|i| ways_to_win(numbers[i], numbers[i + n]))
+            .map(|i| ways_to_win(numbers[i] as f64, numbers[i + n] as f64))
             .product();
         println!("Part {}: {}", part, result);
         input.retain(|c| c != ' ');
