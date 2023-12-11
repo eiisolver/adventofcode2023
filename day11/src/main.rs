26425648fd19bfd320d042bb6702be35d919d732
Louis Verhaard <louis@shortestpath.se>
2023-12-11 17:29:46 +0100

    Day 11 made a bit nicer

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -2,7 +2,7 @@ use std::collections::HashSet;
 use itertools::Itertools;
 use std::fs;
 
-fn get_map(set: &HashSet<i32>, expansion: &i32) -> Vec<i32> {
+fn get_map(set: &HashSet<i64>, expansion: &i64) -> Vec<i64> {
     let max = set.iter().max().unwrap().clone();
     let mut v = 0;
     let mut result = Vec::new();
@@ -18,16 +18,13 @@ fn get_map(set: &HashSet<i32>, expansion: &i32) -> Vec<i32> {
 }
 fn main() {
     let input = fs::read_to_string("input.txt").unwrap();
-    let mut galaxies: Vec<(i32, i32)> = Vec::new();
-    let mut h: i32 = 0;
-    for line in input.lines() {
-        let cols: Vec<i32> = line.chars().enumerate()
-                .filter(|(_, b)| *b == '#')
-                .map(|(a, _)| a as i32).collect();
-        for c in cols {
-            galaxies.push((h, c));
+    let mut galaxies = Vec::new();
+    for (row, line) in input.lines().enumerate() {
+        for (col, c) in line.chars().enumerate() {
+            if c == '#' {
+                galaxies.push((row as i64, col as i64));
+            }
         }
-        h += 1;
     }
     let mut used_cols = HashSet::new();
     let mut used_rows = HashSet::new();
@@ -41,8 +38,8 @@ fn main() {
         let col_map = get_map(&used_cols, &expansion);
         let mut result: i64 = 0;
         for ((r1, c1), (r2, c2)) in galaxies.iter().tuple_combinations() {
-            result += ((row_map[*r1 as usize] - row_map[*r2 as usize]).abs() 
-                    + (col_map[*c1 as usize] - col_map[*c2 as usize]).abs()) as i64;
+            result += (row_map[*r1 as usize] - row_map[*r2 as usize]).abs() 
+                    + (col_map[*c1 as usize] - col_map[*c2 as usize]).abs();
         }
         println!("Part {}: {}", part, result);
     }
